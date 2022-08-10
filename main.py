90e20dc44750a949dd6929b94e92430318cbbaa3
Mikhail <misha.shabalin@mail.ru>
2022-08-10 22:11:47 +0300

    Задание2

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if type(grade) != str:
+            return f'\"{grade}\" не является оценкой'
+        elif grade < 0 or grade > 10:
+            return f'\"{grade}\" не является баллом в пределах от 1 до 10'
+        elif isinstance(lecturer, Lecturer) and course in self.courses_in_progress \
+                and course in lecturer.courses_attached:
+            if course in lecturer.lectures_grades:
+                lecturer.lectures_grades[course] += [grade]
+            else:
+                lecturer.lectures_grades[course] = [grade]
+        else:
+            return f'Студент не посещает курс {grade}, значит он не может ' \
+                   f'оценить текущего преподавателя преподавателя'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,15 +29,20 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lectures_grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) and course in self.courses_attached \
+                and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
-            return 'Ошибка'
-
-class Lecturer(Mentor):
-
-class Reviewer():
+            return f'Данный преподаватель не может проверить курс {course}'
